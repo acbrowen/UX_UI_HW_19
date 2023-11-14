025eb8c748386e9d0350e71d1af9eb756931b72a
acbrowen <135199693+acbrowen@users.noreply.github.com>
2023-11-14 07:42:04 -0500

    Skills Accordion Added
    Default state open

---
+++ js/index.js
@@ -0,0 +1,44 @@
+// Hide/Show Skills under SubHeadings
+$('#UX-skillsHeading').on("click", function(){
+    $('#UX-skillsCollapse').slideToggle();
+  });
+
+$('#UI-skillsHeading').on("click", function(){
+    $('#UI-skillsCollapse').slideToggle();
+  });
+
+$('#Interpersonal-skillsHeading').on("click", function(){
+    $('#Interpersonal-skillsCollapse').slideToggle();
+  });
+
+$('#Cognitive-skillsHeading').on("click", function(){
+    $('#Cognitive-skillsCollapse').slideToggle();
+  });
+
+$('#Communication-skillsHeading').on("click", function(){
+    $('#Communication-skillsCollapse').slideToggle();
+  });
+
+$('#Design-skillsHeading').on("click", function(){
+    $('#Design-skillsCollapse').slideToggle();
+  });
+
+$('#Web-skillsHeading').on("click", function(){
+    $('#Web-skillsCollapse').slideToggle();
+  });
+
+$('#Data-skillsHeading').on("click", function(){
+    $('#Data-skillsCollapse').slideToggle();
+  });
+
+  $('#SpecAnalytics-skillsHeading').on("click", function(){
+    $('#SpecAnalytics-skillsCollapse').slideToggle();
+  });
+
+$('#Survey-skillsHeading').on("click", function(){
+    $('#Survey-skillsCollapse').slideToggle();
+  });
+
+$('#Recruitment-skillsHeading').on("click", function(){
+    $('#Recruitment-skillsCollapse').slideToggle();
+  });
