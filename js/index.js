d06754a49e2ce3d613f96ee4bdaa779e8f970126
acbrowen <135199693+acbrowen@users.noreply.github.com>
2023-11-19 11:26:19 -0500

    UI Interaction created
    Much additional polishing in other sections (e.g., button styles) also.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-// Hide/Show Skills under SubHeadings
+
+
+
 $('#UX-skillsHeading').on("click", function(){
     $('#UX-skillsCollapse').slideToggle();
   });
@@ -42,3 +44,42 @@ $('#Survey-skillsHeading').on("click", function(){
 $('#Recruitment-skillsHeading').on("click", function(){
     $('#Recruitment-skillsCollapse').slideToggle();
   });
+
+$('#navButton-AboutMe').on("click", function(){
+    $('#Recruitment-skillsCollapse').slideToggle();
+  });
+
+
+// Hide/Show Skills under SubHeadings
+
+
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
+
+$("#navButton-AboutMe").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#heroImg").offset().top
+    }, 2000);
+});
